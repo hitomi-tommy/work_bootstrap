d91528e11d5b5ffbb3d74f563aa9481444880e27
hitomi kanaya <hitomickeykey@gmail.com>
2020-08-26 18:38:03 +0900

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :blogs
-  root to: 'tops#index' 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tops#index'
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
